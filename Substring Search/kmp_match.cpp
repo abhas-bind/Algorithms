6bc3da40257d53d7673536a38f344389ce22109e
abhas-bind <161487143+abhas-bind@users.noreply.github.com>
2024-08-27 16:07:43 +0530

    KMP matching algorithm with 1d longest prefix suffix array

---
+++ Substring Search/kmp_match.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class KmpMatching {
+private:
+    string pattern;
+    vector<int> pi;
+
+    void constructDFA() {
+        for (int i = 1, j; i < pattern.length(); i++) {
+            j = pi[i - 1];
+            while (j && pattern[i] != pattern[j]) j = pi[j - 1];
+            if (pattern[i] == pattern[j]) j++;
+            pi[i] = j;
+        }
+    }
+
+public:
+    KmpMatching(string pattern) : pattern(pattern), pi(pattern.size(), 0) {
+
+    }
+
+    size_t findIn(string& text) const {
+        if (text.length() == 0) return std::string::npos;
+        int patternIndex = 0;
+        
+        for (int i = 0; i < text.length(); i++) {
+            while (j && pattern[j] != text[i]) {
+                j = pi[j - 1];
+            }
+            if (pattern[j] == text[i]) j++;
+            if (j == pattern.length()) return i - pattern.length() + 1;
+        }
+
+        return std::string::npos;
+    }
+
+
+};
+
+
+int main () {
+
+    KmpMatching kmp = KmpMatching("ababc");
+    kmp.findIn("abababac");
+
+    return 0;
+}
